cd62a5d2eee0c78d56da4e33199d4d56946e8ebb
JonasPAlves <jonaspereira4321@gmail.com>
2024-06-02 11:35:55 -0300

    Estruturas Condicionais - IF, Else e Elif

---
+++ Bootcamp Aulas/estruturas_condicionais.py
@@ -0,0 +1,10 @@
+MAIOR_IDADE = 18
+
+idade = int(input("Qual a sua idade?"))
+
+if idade >= MAIOR_IDADE:
+    print ("Ok você está autorizado!")
+else:
+    print ("Você não está autorizado pois é menor de idade! tem apenas:", idade, "anos de idade")
+    
+    
