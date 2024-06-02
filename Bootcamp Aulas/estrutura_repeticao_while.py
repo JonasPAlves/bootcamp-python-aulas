ca5d2bdc202296183d4e82d7a5e67708a66c481d
JonasPAlves <jonaspereira4321@gmail.com>
2024-06-02 14:17:36 -0300

    Utilizando o while, break e continue

--- Bootcamp Aulas/estrutura_repeticao_while.py
+++ Bootcamp Aulas/estrutura_repeticao_while.py
@@ -9,4 +9,20 @@ while opcao != 0: # != sinal de diferente
         print("Exibindo o  Extrato...")
     
     else:
-        print("Obrigado por ser nosso cliente!1")
+        print("Obrigado por ser nosso cliente! ")
+        
+while True: # loop infinito pois sempre será verdadeiro até que tenha uma condição para encerrar o loop
+    numero = pcao = int(input("Informe um número: "))
+    
+    if numero ==10:
+        break # condição para parar o loop
+    print (numero)
+    
+for numero in  range(100):
+    
+    if numero % 2 == 0: # se número dividido por 2 for igual a 0 | números primos
+        continue
+    
+    print (numero, end=" ")
+        
+    
