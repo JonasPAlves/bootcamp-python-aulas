bc5a6dca5872b804665b13026a1bd960b5df3c1d
JonasPAlves <jonaspereira4321@gmail.com>
2024-06-02 13:22:17 -0300

    utilizando estruturas de repetição

--- Bootcamp Aulas/estruturas_repeticao_range.py
+++ Bootcamp Aulas/estruturas_repeticao_range.py
@@ -1,15 +1,6 @@
 
-#for numero in range(0,10): # percorre os números do zero ao 9
- #   print(numero) # O texto será Horizontal 
- #   print(numero, end=" ") #end " " é para que o texto seja vertical 
+for numero in range(0,10): # percorre os números do zero ao 9
+    print(numero, end=" ") #end " " é para que o texto seja vertical 
     
     
-    #tabuada do 1 ao 10:
-
-print("TABUADA com Python.")
-num = int ( input("\nDigite um número inteiro para ver sua tabuada: "))
-print("\nA tabuada de", num, "será apresentada abaixo:")
-print(20*"-")
-for i in range (11):
-  print(num, i, num*i)
-print(20*"-")
+print(numero) # O texto será Horizontal    
