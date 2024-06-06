7e1ab834b45ca1bc9bac3e22ae40556c41e88c5a
JonasPAlves <jonaspereira4321@gmail.com>
2024-06-05 21:48:55 -0300

    realizando testes

--- Bootcamp Aulas/comparadores Python.py
+++ Bootcamp Aulas/comparadores Python.py
@@ -1,5 +1,6 @@
 saldo = 450
 saque = 400
+nome = "Jonas"
 
 print (saldo == saque)
 print (saldo != saque)
@@ -7,3 +8,8 @@ print (saldo > saque)
 print (saldo < saque)
 print (saldo >= saque)
 print (saldo <= saque)
+
+###jonas
+
+if nome == "Jonas":
+    print ("Olá!")
